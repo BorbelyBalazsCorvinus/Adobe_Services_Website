bc0e25e67b6c24d60c180e8d38c8a5c140e12ade
Balazsqt <21balazs12@gmail.com>
2023-12-04 23:28:04 +0100

    Patch

--- js/hamburgermenu.js
+++ js/hamburgermenu.js
@@ -2,8 +2,8 @@ const hamburger = document.querySelector(".hamburger");
 const kismenu = document.querySelector(".kismenu");
 hamburger.addEventListener("click", megnyit);
 kismenu.addEventListener("click", bezar);
-document.addEventListener("click", function (event) {
-    if (!kismenu.contains(event.target) && !hamburger.contains(event.target)) {
+document.addEventListener("click", function (elem) {
+    if (!kismenu.contains(elem.target) && !hamburger.contains(elem.target)) {
         kismenu.classList.remove("lenyilt");
         hamburger.classList.remove("lenyilt");
     }
