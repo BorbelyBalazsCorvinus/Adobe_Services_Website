c0313a2c31a6b40bd0c26f1624809109a1bb99e7
Balazsqt <21balazs12@gmail.com>
2023-12-05 11:01:46 +0100

    Patch

--- js/visszajelzes.js
+++ js/visszajelzes.js
@@ -1,19 +1,19 @@
 document.addEventListener('DOMContentLoaded', function () {
     const ertek = document.getElementById('rating');
     ertek.addEventListener('input', function () {
-        if(ertek.value==1){
+        if(ertek.value==0){
             ertekeles="Nagyon nem tetszik"
         }
-        else if(ertek.value==2){
+        else if(ertek.value==1){
             ertekeles="Nem tetszik"
         }
-        else if(ertek.value==3){
+        else if(ertek.value==2){
             ertekeles="Közömbös"
         }
-        else if(ertek.value==4){
+        else if(ertek.value==3){
             ertekeles="Tetszik"
         }
-        else if(ertek.value==5){
+        else if(ertek.value==4){
             ertekeles="Nagyon tetszik"
         }
         ertek.value;
